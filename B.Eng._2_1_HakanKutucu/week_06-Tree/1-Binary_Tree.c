fa3cb5bf5de52092812907a9b055c0435e8f690b
Abdullah Ergül <abdullahergul23@gmail.com>
2022-11-16 00:59:25 +0300

    Upload From PC

--- B.Eng._2_1_HakanKutucu/week_06-Tree/1-Binary_Tree.c
+++ B.Eng._2_1_HakanKutucu/week_06-Tree/1-Binary_Tree.c
@@ -12,6 +12,10 @@ typedef struct node *BTREE;
 
 BTREE new_node(int);
 BTREE insert(BTREE, int);
+void insertVoid(BTREE, int);
+void preorder(BTREE);
+void inorder(BTREE);
+void postorder(BTREE);
 
 int main(){
     // * Get number from user till enter -1
@@ -24,6 +28,16 @@ int main(){
         scanf("%d",&i);
     }
 
+    scanf("%d",&i);
+    while(i != -1){
+        insertVoid(myRoot,i);
+        scanf("%d",&i);
+    }
+
+    printf("\nPreordered BST  = "); preorder(myRoot);
+    printf("\nInordered BST   = "); inorder(myRoot);
+    printf("\nPostordered BST = "); postorder(myRoot);
+
     return 0;
 }
 
@@ -33,6 +47,21 @@ BTREE new_node(int data){
     p->data= data;
     p->left= p->right= NULL;
     return p;
+}
+
+void insertVoid(BTREE *root, int x){
+    if(root->right == NULL && x > root->data || root->left == NULL && x < root->data){
+        if(x < root->data)
+            root->left= new_node(x);
+        else
+            root->right= new_node(x);
+    }
+    else{
+        if(x < root->data)
+            insertVoid(root->left,x);
+        else
+            insertVoid(root->right,x);
+    }
 }
 
 BTREE insert(BTREE root, int x){
@@ -45,4 +74,28 @@ BTREE insert(BTREE root, int x){
             root->right= insert(root->right,x);
     }
     return root;
+}
+
+void preorder(BTREE root){
+    if(root != NULL){
+        printf("%d ",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void inorder(BTREE root){
+    if(root != NULL){
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(BTREE root){
+    if(root != NULL){
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ",root->data);
+    }
 }
