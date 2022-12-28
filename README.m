0f971d6aae7b41f801b69556445b286c4fdc0d4d
Abdullah Ergül <45385505+abdullah-ergul@users.noreply.github.com>
2022-12-29 00:31:25 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,2 @@
-A Github file for Data Structures course which are my 2nd year B.Eng. degree course at Karabuk University.
+A Github file for Data Structures course which are my 2nd years 1st semester B.Eng. degree course at Karabuk University.
 >> Teacher: Hakan KUTUCU | Lab Asistant: Saliha OZGUNGOR
