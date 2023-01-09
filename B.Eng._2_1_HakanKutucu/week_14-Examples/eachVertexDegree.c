50c1900ac7e31249ac1cf526a25e573e382ded59
Abdullah Ergül <abdullahergul23@gmail.com>
2023-01-09 17:58:22 +0300

    Update eachVertexDegree.c

--- B.Eng._2_1_HakanKutucu/week_14-Examples/eachVertexDegree.c
+++ B.Eng._2_1_HakanKutucu/week_14-Examples/eachVertexDegree.c
@@ -1,5 +1,6 @@
 /*
-    Write a program that prints out the degree of each vertex in an undirected graph which is represented by adjency matrix.
+    1- Write a program that prints out the degree of each vertex in an undirected graph which is represented by adjency matrix.
+    2- Write a function that returns the number of edges in an undirected graphwhich is represented by adjacency matrix.
 */
 
 #include <stdio.h>
@@ -7,21 +8,27 @@
 
 
 int degree(int A[6][6], int, int);
+int edges(int A[6][6], int);
 
 
 int main() {
     int i, matrix[6][6] = {
-        {0,1,0,1,0,1},
-        {1,0,1,1,1,0},
-        {0,1,0,0,1,1},
-        {1,1,0,0,0,1},
-        {0,1,1,0,0,0},
-        {1,0,1,1,0,0}
+        {0,1,0,0,1,0},
+        {1,0,1,1,0,0},
+        {0,1,0,1,1,1},
+        {0,1,1,0,1,0},
+        {0,0,1,1,0,1},
+        {0,0,1,0,1,0}
     };
 
-    for(i=0 ; i<6 ; i++) {
+    // 1
+    for(i=0 ; i<6 ; i++)
         printf("%d. vertex's degree: %d\n", i+1, degree(matrix,i,6));
-    }
+
+    printf("\n--------------------------------\n\n");
+
+    //2
+    printf("Number of edges on matrix: %d", edges(matrix, 6));
 
     return 0;
 }
@@ -32,5 +39,15 @@ int degree(int A[6][6], int v, int n) {
     for(i=0 ; i<n ; i++)
         result += A[v][i];
 
+    return result;
+}
+
+int edges(int A[6][6], int n) {
+    int result= 0, i, j;
+
+    for(i=0 ; i<n ; i++)
+        for(j=0 ; j<i ; j++)
+            result += A[i][j];
+    
     return result;
 }
